d8c131a326ca9a4d2fbcd679862693bdf7863903
Hour-Meng <hourmeng.eang26@kit.edu.kh>
2026-04-04 23:00:22 +0700

    first-commit

---
+++ introduction.java
@@ -0,0 +1,30 @@
+// to comment
+
+/*  
+    This  is for multi line command
+*/
+
+// TIPS: We can type "sout" instead of System.out.print()
+public class introduction{ // introduction is the file name
+
+    public static void main(String[] arg){
+        System.out.print("Hello world");  // unlike python, java ended with ;
+        System.out.print("Hello again"); // the problem with this is it still print in the same line, unlike python
+        /*  To fix this is simple we can use:
+            - println
+            - \n inside the print statement like python
+        */
+
+
+        // println
+        System.out.println("Hello World");
+        System.out.println("Hello World again"); // notice that println needed to use for all of the print statement?
+
+        // \n
+        System.out.print("Hello \n");
+        System.out.print("Again"); // while \n use print instead of println
+
+
+    }
+
+}
---
+++ variable.java
@@ -0,0 +1,65 @@
+public class variable{
+    public static void main(String[] args){
+        /*
+            Variables in Java are split into 2 categories:
+
+            1. PRIMITIVE types (store the actual value directly):
+               -byte    : small integer (-128 to 127)
+               -short   : medium integer (-32,768 to 32,767)
+               -int     : integer (most common)
+               -long    : large integer (add L at the end, e.g. 100L)
+               -float   : decimal number (less precise, add F at the end, e.g. 3.14F)
+               -double  : decimal number (more precise, most common for decimals)
+               -char    : single character, uses '' instead of "" (e.g. 'A'), stored as a number internally
+               -boolean : true or false only
+
+            2. REFERENCE types (store a memory address pointing to the actual data):
+               -String  : sequence of characters, uses "" (e.g. "hello")
+               -Arrays  : collection of values (e.g. int[] nums = {1, 2, 3})
+               -Objects : instances of classes (e.g. Scanner, ArrayList, your own classes)
+
+            Key difference:
+               Primitives hold the value itself.
+               References hold the address of where the data lives in memory.
+        */
+
+        /*  2 steps to create a variable:
+            
+            -declaration
+            -assignment
+
+        */
+
+        // int
+        int num = 10; // if add float value there will be an error
+        int score = 90;
+
+        System.out.println(num);
+        System.out.println(score);
+
+        // double
+        double price = 12.9;
+        double height = 180;
+
+        System.out.println(height); // even the number is not a float but since it's in double, the output is 180.0
+        System.out.println(price);
+
+        // char
+        char grade = 'A';
+        char dollar = '$';
+
+        System.out.println(grade);
+        System.out.println(dollar);
+            // char is secretly a number it store the ASCII/Unicode of the charactor
+            // if I use price + dollar it will be 48.9 instead of 12.9$
+        System.out.println(price + "" + dollar); 
+        System.out.println(price + String.valueOf(dollar));
+
+        // bool
+        boolean isStudent = true;
+        boolean isSmart = false; // java use camel case
+
+        System.out.println(isStudent);
+
+    }
+}
